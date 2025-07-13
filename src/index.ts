796b6806dc400abdcf4851e54def4362ca65c7cf
José Cordeiro <joseantcordeiro@gmail.com>
2025-07-13 05:57:34 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { SmartClient } from './client.js'
+export type { SmartClientConfig, SmartConfiguration, TokenResponse } from './types.js'
+export {
+	SmartClientError,
+	SmartClientInitializationError,
+	SmartClientAuthenticationError,
+	SmartClientRequestError,
+} from './types.js'
